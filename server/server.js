4dc765c1717ef68af3743e836121420f78de71c7
sunflower003 <huylee63897@gmail.com>
2025-11-17 14:07:58 +0700

    add 0.0.0.0 into server

--- server/server.js
+++ server/server.js
@@ -67,6 +67,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3369;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
