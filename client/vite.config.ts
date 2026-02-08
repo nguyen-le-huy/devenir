23bb7cbe7e978fd11257229fa1997c101c6fd25f
lehyyy <100980913+sunflower003@users.noreply.github.com>
2026-02-08 14:31:06 +0700

    add docker for dev

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,8 +6,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
     server: {
-        port: 5174,
-        host: 'localhost'
+        port: 5173,
     },
 
     // ============ PRODUCTION OPTIMIZATIONS ============
